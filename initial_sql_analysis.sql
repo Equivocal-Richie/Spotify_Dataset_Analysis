998eb7e7d5b135fecc0821a3d139211189e1f86f
Richard Muchoki <rmrmrichie25@gmail.com>
2025-02-02 00:07:59 +0300

    Checked number of values present in the unique identifiers

--- initial_sql_analysis.sql
+++ initial_sql_analysis.sql
@@ -9,6 +9,10 @@ LIMIT 10;
 SELECT COUNT(*) AS No_Rows
 FROM spotify_data;
 
+-- Let's check the number of unique values in the 'spotify_track_uri' column
+SELECT COUNT(DISTINCT spotify_track_uri) AS unique_spotify_track_uri
+FROM spotify_data; -- output: 16,527
+
 -- Check the number of unique tracks in the data
 SELECT COUNT(DISTINCT track_name) AS unique_tracks
 FROM spotify_data; -- output: 13,643
@@ -16,4 +20,5 @@ FROM spotify_data; -- output: 13,643
 -- Check the distinct platforms listeners use
 SELECT DISTINCT platform
 FROM spotify_data; -- output: 6 unique platforms (android, mac, cast to device, iOS, windows, & web player)
+-- Check how the rank in terms of number of minutes played in the platforms
 
