c0c210265ed3d0582ed38284b1697a5e3a8439bb
Richard Muchoki <rmrmrichie25@gmail.com>
2025-02-06 00:22:01 +0300

    Revamped the cleaning logic

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -1,12 +1,50 @@
 /*
 	** Let's clean & standardise the data first to be in a good format**
 */
+
+/*
+** Duplicate Logic: Duplicate = Same Song (Regardless of Album) **
+An album can share the same name as the tracks eg "The Beattles
+
+-- start_time = end_time - ms_played
+*/
+-- Check for duplicates in the data
+WITH duplicate_cte AS (
+    SELECT
+        spotify_track_uri,
+        ts AS end_time,
+        DATE_ADD(ts, INTERVAL -ms_played / 1000.0 SECOND) AS start_time,  
+        ROUND(ms_played / 60000.0, 2) AS duration,
+        ROW_NUMBER() OVER (PARTITION BY spotify_track_uri, ts, platform, ms_played, track_name, reason_start, reason_end, shuffle, skipped ORDER BY ts) AS rn
+    FROM spotify_data
+)
+SELECT * FROM duplicate_cte 
+WHERE rn > 1;
+
+-- Check for Null Vlaues
+SELECT
+    COUNT(*) AS total_rows,  -- Total number of rows in the table
+    COUNT(CASE WHEN spotify_track_uri IS NULL THEN 1 END) AS spotify_track_uri_nulls,
+    COUNT(CASE WHEN ts IS NULL THEN 1 END) AS ts_nulls,
+    COUNT(CASE WHEN ms_played IS NULL THEN 1 END) AS ms_played_nulls,
+    COUNT(CASE WHEN platform IS NULL THEN 1 END) AS platform_nulls,
+    COUNT(CASE WHEN track_name IS NULL THEN 1 END) AS track_name_nulls,
+    COUNT(CASE WHEN album_name IS NULL THEN 1 END) AS album_name_nulls,
+    COUNT(CASE WHEN artist_name IS NULL THEN 1 END) AS artist_name_nulls,  
+    COUNT(CASE WHEN reason_start IS NULL THEN 1 END) AS reason_start_nulls,
+    COUNT(CASE WHEN reason_end IS NULL THEN 1 END) AS reason_end_nulls,
+    COUNT(CASE WHEN shuffle IS NULL THEN 1 END) AS shuffle_nulls,
+    COUNT(CASE WHEN skipped IS NULL THEN 1 END) AS skipped_nulls
+FROM spotify_data;
+
+
 -- Create a new table to store the clean data
 CREATE TABLE clean_spotify_data (
     spotify_track_url TEXT,
-    ts TIMESTAMP, -- Ensure ts is stored as TIMESTAMP for proper time handling
+    start_time TIMESTAMP,
+    end_time TIMESTAMP, -- Ensure it is stored as TIMESTAMP for proper time handling
     platform VARCHAR(255), 
-    ms_played_minutes DECIMAL(10,2), -- Use DECIMAL for accurate decimal representation
+    duration DECIMAL(10,2), -- Use DECIMAL for accurate decimal representation
     track_name VARCHAR(255),
     artist_name VARCHAR(255),
     album_name VARCHAR(255),
@@ -17,36 +55,22 @@ CREATE TABLE clean_spotify_data (
 );
 
 
--- View the original data 
-SELECT * FROM spotify_data
-LIMIT 5;
-
-
 -- Insert data into the new table with data transformations
 INSERT INTO clean_spotify_data
 SELECT 
     spotify_track_uri AS spotify_track_url, 
-    ts, 
+    DATE_ADD(ts, INTERVAL -ms_played / 1000.0 SECOND) AS start_time,
+    ts AS end_time, 
     platform, 
-    ROUND(ms_played / 60000.0, 2) AS ms_played_minutes, -- Calculate minutes played
+    ROUND(ms_played / 60000.0, 2) AS duration, -- Calculate minutes played
     track_name, 
     artist_name, 
     album_name, 
-    CASE 
-        WHEN reason_start = '' THEN NULL 
-        ELSE reason_start 
-    END AS reason_start, 
-    CASE 
-        WHEN reason_end = '' THEN NULL 
-        ELSE reason_end 
-    END AS reason_end, 
-    CASE 
-        WHEN shuffle = 1 THEN TRUE 
-        ELSE FALSE 
-    END AS Shuffle, 
-    CASE 
-        WHEN skipped = 1 THEN TRUE 
-        ELSE FALSE 
-    END AS Skipped
+	reason_start, 
+	reason_end, 
+    shuffle, 
+    skipped
 FROM 
     spotify_data;
+    
+       
